4241462065055e17f344e249b501a089c11d6a9e
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2017-11-08 17:13:10 +0200

    Update s3 conn for demo

--- openprocurement/storage/s3/__init__.py
+++ openprocurement/storage/s3/__init__.py
@@ -1,4 +1,4 @@
-from boto.s3.connection import S3Connection
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from openprocurement.storage.s3.storage import S3Storage
 
 
@@ -7,10 +7,14 @@ def includeme(config):
     if ('s3.access_key' in settings and 's3.secret_key' in settings and
             's3.bucket' in settings):
         # S3 connection
-        if 's3.host' in settings:
-            connection = S3Connection(settings['s3.access_key'],
-                                      settings['s3.secret_key'],
-                                      host=settings['s3.host'])
+        if 's3.host' in settings and 's3.port' in settings:
+            connection = S3Connection(
+                aws_access_key_id=settings['s3.access_key'],
+                aws_secret_access_key=settings['s3.secret_key'],
+                is_secure=False,
+                host=settings['s3.host'],
+                port=int(settings['s3.port']),
+                calling_format=OrdinaryCallingFormat())
         else:
             connection = S3Connection(settings['s3.access_key'],
                                       settings['s3.secret_key'])
